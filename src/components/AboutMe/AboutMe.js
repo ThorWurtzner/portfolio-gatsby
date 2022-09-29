e48b117f05f3fd761c236d1c392e18dfbbff1b39
Thor W <6273158@rts-365.dk>
2022-09-29 21:05:16 +0200

    description updated, added title and favicon

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -16,16 +16,19 @@ export default function AboutMe() {
             <div className='aboutMe__content'>
                 <div className='aboutMe__portraitWrapper'>
                     <StaticImage className='portrait' src='../../images/portrait.png' alt='portrait' placeholder='blurred'/>
-                    <p>Thor Würtzner</p>
-                    <p>Web Developer <br/> Roskilde Technical College</p>
-                    <p>thorwurtzner@gmail.com</p>
-                    <p>Aarhus - Denmark</p>
+                    <div className='aboutMe__portraitWrapper__description'>
+                        <h2>Thor Würtzner</h2>
+                        <div>
+                            <p>- Web Developer -</p>
+                            <p>Roskilde Technical College</p>
+                        </div>
+                    </div>
                 </div>
 
                 <div className='aboutMe__content__inner'>
                     <h2>Hey again!</h2>
-                    <p>So, I'm a developer living in Aarhus, Denmark. But originally from Copenhagen. <br /> I used to sell a massive amount of suits before all of this developer stuff - in fact I was the top seller of the brand one month! <br/> But yeah, because of that I make sure to never let my customers or partners leave dissatisfied. I really enjoy creating responsive websites with optimal user experience that achieves your goals as a business. Not only do I make sure your site reaches the widest audience possible, I make it look damn good as well.</p>
-                    <h2>Some of my illustrations and designs:</h2>
+                    <p>I'm a developer living in Aarhus, Denmark. Originally from Copenhagen. <br /> I used to sell suits before all of this developer stuff - in fact I was the top seller of the brand one month! <br/> But yeah, because of that I make sure to never let my customers or partners leave dissatisfied. I really enjoy creating responsive websites with an user experience that achieves your goals as a business. Not only do I make sure your site reaches the widest audience possible, I make it look damn good as well.</p>
+                    <h2>Some of my other illustrations and designs:</h2>
                     <div className='aboutMe__illustrations'>
                         <StaticImage className='aboutMe__illustrations__img' src='../../images/illustrations/copenhagen.png' alt='' placeholder='blurred'/>
                         <StaticImage className='aboutMe__illustrations__img' src='../../images/illustrations/avis.png' alt='' placeholder='blurred'/>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,23 @@
 import * as React from "react"
 import "./index.scss";
 import Introduction from "../components/Introduction/Introduction"
-import Navigation from "../components/Navigation/Navigation";
+// import Navigation from "../components/Navigation/Navigation";
 import Footer from "../components/Footer/Footer";
 import AboutMe from "../components/AboutMe/AboutMe";
 import Mywork from "../components/MyWork/MyWork";
-import RoadMap from "../components/RoadMap/RoadMap";
+import { Helmet } from "react-helmet";
+// import RoadMap from "../components/RoadMap/RoadMap";
+import favicon from "../images/favicon.png"
 
 export default function Index() {
   return (
     <div className="App">
+
+      <Helmet>
+        <title>Thor Würtzner - Portfolio</title>
+        <link rel="icon" type="image/png" href={favicon} />
+      </Helmet>
+      
       <Introduction />
       <main className="main" id="main">
         {/* <Navigation /> */}
