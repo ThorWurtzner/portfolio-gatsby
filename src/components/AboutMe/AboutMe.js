3b1cb0b9a3565f431bdc3f72e223a4bb18a6415d
Thor W <6273158@rts-365.dk>
2023-10-15 12:52:49 +0200

    changed to production site

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -38,16 +38,16 @@ export default function AboutMe() {
                         <a href="https://thorwurtzner.dk/static/16e917eb50baa80ae621bd1fca6299d8/1ed78/copenhagen.webp" target='_blank'>
                             <StaticImage className='aboutMe__illustrations__img' src='../../images/illustrations/copenhagen.png' alt='' placeholder='blurred'/>
                         </a>
-                        <a href="http://localhost:8000/static/f3e3374c8b2d01c2a56cef31216acb98/0dd7a/spotify1.webp" target='_blank'>
+                        <a href="https://thorwurtzner.dk/static/f3e3374c8b2d01c2a56cef31216acb98/0dd7a/spotify1.webp" target='_blank'>
                             <StaticImage className='aboutMe__illustrations__img' src='../../images/illustrations/spotify1.png' alt='' placeholder='blurred'/>
                         </a>
-                        <a href="http://localhost:8000/static/6fb4254e58e00502d128a4fb914b24f4/0dd7a/spotify2.webp" target='_blank'>
+                        <a href="https://thorwurtzner.dk/static/6fb4254e58e00502d128a4fb914b24f4/0dd7a/spotify2.webp" target='_blank'>
                             <StaticImage className='aboutMe__illustrations__img' src='../../images/illustrations/spotify2.png' alt='' placeholder='blurred'/>
                         </a>
-                        <a href="http://localhost:8000/static/6b671c447312a00c0f6fb2882a0accbc/ba936/avis.webp" target='_blank'>
+                        <a href="https://thorwurtzner.dk/static/6b671c447312a00c0f6fb2882a0accbc/ba936/avis.webp" target='_blank'>
                             <StaticImage className='aboutMe__illustrations__img' src='../../images/illustrations/avis.png' alt='' placeholder='blurred'/>
                         </a>
-                        <a href="http://localhost:8000/static/00a42db3f1c8038346469eb9b5bd7293/a7cad/storage.webp" target='_blank'>
+                        <a href="https://thorwurtzner.dk/static/00a42db3f1c8038346469eb9b5bd7293/a7cad/storage.webp" target='_blank'>
                             <StaticImage className='aboutMe__illustrations__img' src='../../images/illustrations/storage.png' alt='' placeholder='blurred'/>
                         </a>
                     </div>
